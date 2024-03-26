c7fd1b4f02380563de96f4eac8abe443bcb47f69
Sulton <sultonalidrus5@gmail.com>
2024-03-27 05:48:19 +0700

    style : seo create event

--- src/app/(root)/create-event/page.tsx
+++ src/app/(root)/create-event/page.tsx
@@ -1,14 +1,15 @@
 import FormCreateEvent from "@/components/template/form/formevent/FormCreateEvent";
 import { getServerAuthSession } from "@/server/auth";
-import { redirect } from "next/navigation";
+import { type Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Make your event in Karcisku",
+  description: "Reserve, Create, Celebrate: Your Event, Your Rules!",
+};
 
 const page = async () => {
   const data = await getServerAuthSession();
-
-  if (!data) {
-    redirect("/login");
-  }
 
   return (
     <div className="container mt-5 max-w-screen-lg">
